b7eb93db55931d656257a76ede903a3a66d97a74
paul <birnbaumpaul@yahoo.fr>
2022-02-08 19:08:36 +1100

    Video section in progress

--- next-website/components/Video.js
+++ next-website/components/Video.js
@@ -3,9 +3,9 @@ import style from '../styles/Home.module.scss'
 
 export default function Video() {
   return (
-    <section> 
+    <section className={style.video}> 
       <article>
-        <h1 className='text-2xl font-extrabold'>Nostrud dolore sit velit voluptate esse duis ea laboris fugiat incididunt amet.  </h1>
+        <h1 className='text-2xl font-extrabold'>Nostrud  incididunt amet.  </h1>
         <p>Quam viverra orci sagittis eu volutpat odio. Et ligula ullamcorper malesuada proin libero nunc</p>
       </article>
       <iframe src="https://www.youtube.com/embed/fbIAvP5Gdzs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
